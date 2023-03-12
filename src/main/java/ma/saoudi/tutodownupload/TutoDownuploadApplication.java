ceefa389579cf091257affbd71fd3d963d8aec23
RachidSAOUDI <rachid.saoudi@usmba.ac.ma>
2023-03-12 13:09:06 +0100

    initial commit

---
+++ src/main/java/ma/saoudi/tutodownupload/TutoDownuploadApplication.java
@@ -0,0 +1,13 @@
+package ma.saoudi.tutodownupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoDownuploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutoDownuploadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/saoudi/tutodownupload/TutoDownuploadApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.saoudi.tutodownupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutoDownuploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
